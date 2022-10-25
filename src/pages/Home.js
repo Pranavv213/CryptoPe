c8f73c0ff71d78a9eb0c4ce57a2b9467057dbbda
Pranavv213 <98053577+Pranavv213@users.noreply.github.com>
2022-10-25 15:36:11 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ import CryptoPe from './Cryp.png'
 
 
 const Home = () => {
-  const q=[1,2,3,4,5,6,7,8]
+  const q=[1,2,3,4,5,6,7,8,9]
   const Web3Api = useMoralisWeb3Api();
   const contractProcessor = useWeb3ExecuteFunction();
   const resolveLink = (url) => {
